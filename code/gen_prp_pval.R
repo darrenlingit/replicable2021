a0f005c30c2ca806af3eefff6d0ddec4133b06be
Darren Lin <54636352+dlin33@users.noreply.github.com>
2021-08-03 21:45:16 -0700

    Create gen_prp_pval.R
    Function for getting prp values

---
+++ code/gen_prp_pval.R
@@ -0,0 +1,107 @@
+# libraries and other things
+library(PRP)
+library(rstudioapi)
+library(mamba)
+library(data.table)
+library(parallel)
+
+set.seed(2021)
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+create_prp_data <- function(noutlier_rate = 0.975) {
+  # text for names
+  text_rate <- gsub("\\.", "", as.character(noutlier_rate*100))
+  
+  # files we need
+  mamba_data_file <- paste("../data/mamba_data_p",
+                           text_rate,
+                           ".rda",
+                           sep = "")
+  sim_mamba_mod_file <- paste("../data/sim_mamba_mod_p",
+                              text_rate,
+                              ".rda",
+                              sep = "")
+  
+  # files we want to save data/results to
+  post_prp_data_file <- paste("../data/post_prp_data",
+                              text_rate,
+                              ".rda",
+                              sep = "")
+  post_prp_data_pval_file <- paste("../data/post_prp_data_pval_p",
+                                   text_rate,
+                                   ".rda",
+                                   sep = "")
+  
+  post_prp_data_ch_file <- paste("../data/post_prp_data_p",
+                                 text_rate,
+                                 ".rda",
+                                 sep = "")
+  
+  
+  # loading in or creating data
+  if (file.exists(mamba_data_file) &&
+      file.exists(sim_mamba_mod_file) ) {
+    
+    load(file = mamba_data_file)
+    load(file = sim_mamba_mod_file)
+    
+    snpeffect <- mamba_data$betajk
+    snpvar <- mamba_data$sjk2
+    
+  } else {
+    # simulate data with MAMBA
+    mamba_data <- generate_data_mamba(lambda = noutlier_rate)
+    
+    save(mamba_data,
+         file = mamba_data_file)
+    
+    
+    # save effects and variance
+    snpeffect <- mamba_data$betajk
+    snpvar <- mamba_data$sjk2
+    
+    # fit mamba model
+    sim_mod <- mamba(betajk = snpeffect, sjk2 = snpvar)
+    save(sim_mod,
+         file = sim_mamba_mod_file)
+    
+  }
+  
+  
+  # list of effects and list of se
+  # each item is a row from the data
+  list_eff = lapply(seq_len(nrow(snpeffect)),
+                    function(x) {
+                      snpeffect[x,]
+                    })
+  list_se = lapply(seq_len(nrow(snpvar)),
+                   function(x) {
+                     sqrt(snpvar[x,])
+                   })
+  
+  
+  # fitting to posterior_prp function
+  print("Now applying posterior_prp function.")
+  post_prp_data = t(mapply(posterior_prp,
+                           beta = list_eff,
+                           se = list_eff))
+  
+  post_prp_data <- as.data.frame(post_prp_data)
+  
+  post_prp_data_pval <- as.numeric(post_prp_data$pvalue)
+  
+  post_prp_data_ch <- apply(post_prp_data,
+                            MARGIN = 2,
+                            FUN = as.character)
+  
+  
+  
+  # saving functions
+  save(post_prp_data, file = post_prp_data_file)
+  save(post_prp_data_pval, file = post_prp_data_pval_file)
+  write.csv(post_prp_data_ch, post_prp_data_ch_file, row.names = FALSE)
+  
+}
+
+#create_prp_data(noutlier_rate = 0.1)
