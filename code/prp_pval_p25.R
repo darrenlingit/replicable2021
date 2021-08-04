6bda4aac244fe0baa4954180e7ca24a96078e1de
Darren Lin <54636352+dlin33@users.noreply.github.com>
2021-08-03 21:51:19 -0700

    Create prp_pval_p25.R

---
+++ code/prp_pval_p25.R
@@ -0,0 +1,68 @@
+# libraries and other things
+set.seed(2021)
+
+library(PRP)
+library(rstudioapi)
+library(mamba)
+library(data.table)
+library(parallel)
+
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+noutlier_rate = 0.25
+
+# loading in or creating data
+if (file.exists("../data/mamba_data_p25.rda") &&
+    file.exists("../data/sim_mamba_mod_p25.rda") ) {
+  
+  load(file = "../data/mamba_data_p25.rda")
+  load(file = "../data/sim_mamba_mod_p25.rda")
+  
+  snpeffect <- mamba_data_p25$betajk
+  snpvar <- mamba_data_p25$sjk2
+  
+} else {
+  # simulate data with MAMBA
+  mamba_data_p25 <- generate_data_mamba(lambda = noutlier_rate)
+  save(mamba_data_p25, file = "../data/mamba_data_p25.rda")
+  
+  # save effects and variance
+  snpeffect <- mamba_data_p25$betajk
+  snpvar <- mamba_data_p25$sjk2
+  
+  # fit mamba model
+  sim_mod_p25 <- mamba(betajk = snpeffect, sjk2 = snpvar)
+  save(sim_mod_p25, file = "../data/sim_mamba_mod_p25.rda")
+  
+}
+
+
+# list of effects and list of se
+# each item is a row from the data
+list_eff = lapply(seq_len(nrow(snpeffect)),
+                  function(x) {
+                    snpeffect[x,]
+                  })
+list_se = lapply(seq_len(nrow(snpvar)),
+                 function(x) {
+                   sqrt(snpvar[x,])
+                 })
+
+
+# fitting to posterior_prp function
+print("Now applying posterior_prp function.")
+post_prp_data_p25 = t(mapply(posterior_prp,
+                         beta = list_eff,
+                         se = list_eff))
+
+post_prp_data_p25 <- as.data.frame(post_prp_data_p25)
+
+post_prp_data_pval_p25 <- as.numeric(post_prp_data_p25$pvalue)
+
+post_prp_data_ch_p25 <- apply(post_prp_data_p25,
+                              MARGIN = 2,
+                              FUN = as.character)
+
+
+save(post_prp_data_p25, file = "../data/post_prp_data_p25.rda")
+save(post_prp_data_pval_p25, file = "../data/post_prp_data_pval_p25.rda")
+write.csv(post_prp_data_ch_p25, "../data/post_prp_data_p25.csv", row.names = FALSE)
