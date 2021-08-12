259b1e7d007a53939134346a9130fe0decaaea05
Darren Lin <54636352+dlin33@users.noreply.github.com>
2021-08-12 00:41:28 -0700

    Updated for GWAS meta data
    Data cleaning for GWAS meta files
    
    Updated giant_replicable function to work with prior_prp

--- main_files/data/giant_replicable.R
+++ main_files/data/giant_replicable.R
@@ -8,7 +8,13 @@ library(dplyr)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-giant_replicable <- function(beta, se, prp_file, prp_pval_file, mamba_data_file) {
+giant_replicable <- function(beta,
+                             se,
+                             prp_file,
+                             prp_pval_file,
+                             mamba_data_file,
+                             prior_post = "posterior") {
+  
   # list of effects and list of se
   # each item is a row from the data
   # leaves out NA values
@@ -26,25 +32,44 @@ giant_replicable <- function(beta, se, prp_file, prp_pval_file, mamba_data_file)
                      #se <- se[is.infinite(se) == FALSE]
                    })
   
+  # PRP package functions
+  if (prior_post == "posterior") {
+    
+    # fitting to posterior_prp function
+    print("Now applying posterior_prp function.")
+    post_prp_data = t(mapply(posterior_prp,
+                             beta = list_eff,
+                             se = list_se))
+    
+    post_prp_data <- as.data.frame(post_prp_data)
+    
+    post_prp_data_pval <- as.numeric(post_prp_data$pvalue)
+    
+    # saving prp data
+    save(post_prp_data, file = prp_file)
+    save(post_prp_data_pval, file = prp_pval_file)
+    
+  } else if (prior_post == "prior") {
+    
+    # fitting to prior_prp function
+    print("Now applying prior_prp function.")
+    prior_prp_data = t(mapply(prior_prp,
+                              beta = list_eff,
+                              se = list_se))
+    
+    prior_prp_data <- as.data.frame(prior_prp_data)
+    
+    prior_prp_data_pval <- as.numeric(prior_prp_data$pvalue)
+    
+    # saving prp data
+    save(prior_prp_data, file = prp_file)
+    save(prior_prp_data_pval, file = prp_pval_file)
+    
+  }
   
-  # fitting to posterior_prp function
-  print("Now applying posterior_prp function.")
-  post_prp_data = t(mapply(posterior_prp,
-                           beta = list_eff,
-                           se = list_se))
-  
-  post_prp_data <- as.data.frame(post_prp_data)
-  
-  post_prp_data_pval <- as.numeric(post_prp_data$pvalue)
-  
-  # saving prp data
-  save(post_prp_data, file = prp_file)
-  save(post_prp_data_pval, file = prp_pval_file)
   
   
-  
-  
-  # now fitting mamba
+  # MAMBA fitting
   print("Now applying MAMBA library.")
   mamba_data <- mamba(beta, se^2)
   
